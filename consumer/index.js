3cab2f27d45c1cb429a51d10ad0e416bfd74bab2
Toon Sevrin <twan123@live.be>
2016-10-16 20:18:13 +0000

    Testing stuff

--- consumer/index.js
+++ consumer/index.js
@@ -1,8 +1,9 @@
 /* eslint-env node */
 module.exports = function(context, req) {
+    var content = {msg: "helloworld!"};
     context.res = {
             // status: 200, /* Defaults to 200 */
-            body: {msg : "Helloworld!"}
+            body: content;
     };
     context.done();
 }
