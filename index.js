62675c17d64e7b16550a24739badf51e9ed8c2b6
Toon Sevrin <twan123@live.be>
2016-10-16 20:07:29 +0000

    Fixed typo

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ module.exports = function(context, req) {
     context.res = {
             // status: 200, /* Defaults to 200 */
             body: {msg : "Helloworld!"}
-        };
+    };
     context.done();
+}
+
